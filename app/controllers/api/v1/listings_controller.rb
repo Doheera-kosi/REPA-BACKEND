5db3851a338b6c46b21c3550254751e8da16872a
Doheera-kosi <kupourevans99@gmail.com>
2023-04-25 21:20:25 +0000

    Add methods for listings controller

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,10 +1,32 @@
 class Api::V1::ListingsController < ApplicationController
+  before_action :set_listing, only: [:destroy]
+
   def index
+    @listings = Listing.all
+    render json: @listings
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      render json: @listing, status: :created
+    else
+      render json: @listing.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @listing.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:user_id, :property_id)
   end
 end
