f34812fbddcdcdf62dbda8723efdc28e8c2d3caf
Doheera-kosi <kupourevans99@gmail.com>
2023-04-25 21:19:23 +0000

    Add controller methods to properties and users

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,16 +1,44 @@
 class Api::V1::PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :update, :destroy]
+
   def index
+    @properties = Property.all
+    render json: @properties
   end
 
   def show
+    render json: @property
   end
 
   def create
+    @property = Property.new(property_params)
+    if @property.save
+      render json: @property, status: :created
+    else
+      render json: @property.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @property.update(property_params)
+      render json: @property
+    else
+      render json: @property.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @property.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name, :price, :description, :address, :city, :state, :zip)
   end
 end
