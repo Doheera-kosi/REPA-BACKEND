5923b1821cfdea079a03552cebc10355e582d8d3
Doheera-kosi <kupourevans99@gmail.com>
2023-04-26 22:09:03 +0000

    Add validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,9 @@
 class Property < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip_code, presence: true
 end
