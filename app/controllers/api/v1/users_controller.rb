98d8286bad9d2bce60039701a7550df6fca0b2d0
Doheera-kosi <kupourevans99@gmail.com>
2023-04-26 21:11:10 +0000

    Update login action method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
     if user && user.authenticate(params[:password])
       token = JWT.encode({ user_id: user.id }, Rails.application.secrets.secret_key_base)
 
-      render json: { token }
+      render json: { token: token }
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
