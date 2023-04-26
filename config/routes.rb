2ff9d118108fd801687e485a1863a2ad03d257d8
Doheera-kosi <kupourevans99@gmail.com>
2023-04-26 22:56:28 +0000

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :properties
+      resources :properties do
+        collection do
+          get 'search'
+        end
+      end
+      
       resources :users do
         post 'login', on: :collection
       end
+      
       resources :listings, only: [:index, :create, :destroy]
     end
   end
