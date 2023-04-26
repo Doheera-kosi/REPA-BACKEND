f34d39a0aff7f604ef76523230ebe0cf90610661
Doheera-kosi <kupourevans99@gmail.com>
2023-04-26 23:24:48 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,3 @@
-# Rails.application.routes.draw do
-#   namespace :api do
-#     namespace :v1 do
-#       get 'listings/index'
-#       get 'listings/create'
-#       get 'listings/destroy'
-#       get 'users/index'
-#       get 'users/show'
-#       get 'users/create'
-#       get 'users/update'
-#       get 'users/destroy'
-#       get 'properties/index'
-#       get 'properties/show'
-#       get 'properties/create'
-#       get 'properties/update'
-#       get 'properties/destroy'
-#     end
-#   end
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-#   # Defines the root path route ("/")
-#   # root "articles#index"
-# end
-
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
