74c1416d065e60ca66a3b0e194cb467382e8056d
Doheera-kosi <kupourevans99@gmail.com>
2023-04-26 20:57:18 +0000

    Add user authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  def authenticate(password)
+    if BCrypt::Password.new(self.password_digest) == password
+      self
+    else
+      false
+    end
+  end
 end
