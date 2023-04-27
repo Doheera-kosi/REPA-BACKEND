364bdbf78020305d130b0c1792208759786c1604
Doheera-kosi <kupourevans99@gmail.com>
2023-04-27 00:55:14 +0000

    Update new user creation method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,10 +24,11 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      render json: @user, status: :created
+      render json: { message: 'Registration successful' }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
