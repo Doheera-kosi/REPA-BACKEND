7a668d78dbc31949a43e85be316172ec1f682f85
Doheera-kosi <kupourevans99@gmail.com>
2023-04-27 00:59:03 +0000

    Add endpoint for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/register', to: 'registrations#create'
       resources :properties do
         collection do
           get 'search'
