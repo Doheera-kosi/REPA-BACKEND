9a51f17fe6ccfa8a654f89d31f8d4e29bbb326cd
Doheera-kosi <kupourevans99@gmail.com>
2023-04-27 01:30:02 +0000

    Modify sessions_controller method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,12 @@
 # app/controllers/api/v1/sessions_controller.rb
+
 class Api::V1::SessionsController < ApplicationController
     def create
+      @user = User.authenticate(params[:email], params[:password])
+  
+      render json: { message: 'Login successful', user: @user }
+    rescue StandardError => e
+      render json: { error: e.message }, status: :unauthorized
     end
   end
   
